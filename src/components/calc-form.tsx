d50340d795487dc201fc9855364aef811b8d85bb
hossokawa <43284591+hossokawa@users.noreply.github.com>
2025-05-12 21:45:42 -0300

    refactor: substituir componente select por input para os diâmetros

--- src/components/calc-form.tsx
+++ src/components/calc-form.tsx
@@ -6,7 +6,6 @@ import { z } from "zod"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue } from "@/components/ui/select"
 
 const formSchema = z.object({
   diametroSuccao: z.number({ coerce: true }).nonnegative(),
@@ -18,8 +17,8 @@ export function CalculadoraForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      diametroSuccao: 20,
-      diametroRecalque: 20,
+      diametroSuccao: 0,
+      diametroRecalque: 0,
       vazao: 0,
     },
   })
@@ -31,72 +30,57 @@ export function CalculadoraForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="diametroSuccao"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Diâmetro tubulação de sucção</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue="0">
+        <div className="grid grid-cols-4 grid-rows-4 gap-4 space-y-4">
+          <FormField
+            control={form.control}
+            name="diametroSuccao"
+            render={({ field }) => (
+              <FormItem className="col-span-2 w-full">
+                <FormLabel>Diâmetro da tubulação de sucção (mm)</FormLabel>
                 <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Selecione um diâmetro" />
-                  </SelectTrigger>
+                  <Input type="number" placeholder="0" {...field} />
                 </FormControl>
-                <SelectContent className="bg-zinc-800">
-                  <SelectItem value="0">0</SelectItem>
-                  <SelectItem value="20">20</SelectItem>
-                  <SelectItem value="25">25</SelectItem>
-                  <SelectItem value="32">32</SelectItem>
-                  <SelectItem value="40">40</SelectItem>
-                  <SelectItem value="50">50</SelectItem>
-                  <SelectItem value="60">60</SelectItem>
-                  <SelectItem value="75">75</SelectItem>
-                  <SelectItem value="85">85</SelectItem>
-                  <SelectItem value="100">100</SelectItem>
-                  <SelectItem value="150">150</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                Diâmetro da tubulação antes da bomba.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="diametroRecalque"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Diâmetro tubulação de sucção</FormLabel>
-              <FormControl>
-                <Input placeholder="0" {...field} />
-              </FormControl>
-              <FormDescription>
-                Diâmetro da tubulação depois da bomba.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="vazao"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Diâmetro tubulação de sucção</FormLabel>
-              <FormControl>
-                <Input type="number" placeholder="0" {...field} />
-              </FormControl>
-              <FormDescription>
-                Vazão no reservatório em m^3/h.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Calcular</Button>
+                <FormDescription className="flex justify-start">
+                  Diâmetro interno da tubulação antes da bomba.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="diametroRecalque"
+            render={({ field }) => (
+              <FormItem className="col-span-2 w-full">
+                <FormLabel>Diâmetro da tubulação de recalque (mm)</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="0" {...field} />
+                </FormControl>
+                <FormDescription className="flex justify-start">
+                  Diâmetro interno da tubulação depois da bomba.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="vazao"
+            render={({ field }) => (
+              <FormItem className="col-span-2 w-full">
+                <FormLabel>Vazão alvo (m³/h)</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="0" {...field} />
+                </FormControl>
+                <FormDescription className="flex justify-start">
+                  Vazão pretendida no reservatório.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="row-start-4 col-start-2 col-end-4 text-xl bg-blue-700">Calcular</Button>
+        </div>
       </form>
     </Form>
   )
